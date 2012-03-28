67b181579f56f7ec5ac23cba8e96c99a8e2d962c
Nattyseydi <nattyseydi@gmail.com>
2012-03-28 12:08:50 +0000

    setting up groupeomaxi repository

---
+++ sites/all/themes/framework/block.tpl.php
@@ -0,0 +1,13 @@
+<section id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+
+  <?php print render($title_prefix); ?>
+  <?php if (!empty($block->subject)): ?>
+    <h2 <?php print $title_attributes; ?>><?php print $block->subject ?></h2>
+  <?php endif;?>
+  <?php print render($title_suffix); ?>
+
+  <div class="content"<?php print $content_attributes; ?>>
+    <?php print $content ?>
+  </div>
+  
+</section> <!-- /.block -->
---
+++ sites/all/themes/framework/page.tpl.php
@@ -0,0 +1,76 @@
+<div id="wrapper" class="clearfix">
+
+  <div id="skip-link">
+    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+    <?php if ($main_menu): ?>
+      <a href="#navigation" class="element-invisible element-focusable"><?php print t('Skip to navigation'); ?></a>
+    <?php endif; ?>
+  </div>
+
+  <header id="header" role="banner" class="clearfix">
+	<?php if ($logo): ?>
+      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" id="logo">
+        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+      </a>
+    <?php endif; ?>
+    <?php if ($site_name || $site_slogan): ?>
+      <div id="site-name-slogan">
+        <?php if ($site_name): ?>
+          <?php if ($title): ?>
+            <div id="site-name"><strong>
+              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><span><?php print $site_name; ?></span></a>
+            </strong></div>
+          <?php else: /* Use h1 when the content title is empty */ ?>
+            <h1 id="site-name">
+              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><span><?php print $site_name; ?></span></a>
+            </h1>
+          <?php endif; ?>
+        <?php endif; ?>
+        <?php if ($site_slogan): ?>
+          <div id="site-slogan"><?php print $site_slogan; ?></div>
+        <?php endif; ?>
+      </div>
+    <?php endif; ?>
+    <?php print render($page['header']); ?>
+  </header> <!-- /#header -->
+
+  <section id="main" role="main" class="clearfix">
+    <?php print $messages; ?>
+
+		<?php if ($page['content_top']): ?>
+    <aside id="content_top" role="complementary" class="content_top clearfix">
+      <?php print render($page['content_top']); ?>
+    </aside>  <!-- /#content_top -->
+  	<?php endif; ?> 
+
+		<a id="main-content"></a>
+    <?php print render($title_prefix); ?>
+    <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+    <?php print render($title_suffix); ?>
+    <?php if (!empty($tabs['#primary'])): ?><div class="tabs-wrapper clearfix"><?php print render($tabs); ?></div><?php endif; ?>
+    <?php print render($page['help']); ?>
+    <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+    <?php print render($page['content']); ?>
+
+		<?php if ($page['content_bottom']): ?>
+    <aside id="content_bottom" role="complementary" class="content_bottom clearfix">
+      <?php print render($page['content_bottom']); ?>
+    </aside>  <!-- /#content_top -->
+  	<?php endif; ?> 
+
+  </section> <!-- /#main -->
+  
+  <?php if ($page['sidebar_first']): ?>
+    <aside id="sidebar-first" role="complementary" class="sidebar clearfix">
+      <?php print render($page['sidebar_first']); ?>
+    </aside>  <!-- /#sidebar-first -->
+  <?php endif; ?>
+
+  
+
+  <footer id="footer" role="contentinfo" class="clearfix">
+    <?php print render($page['footer']) ?>
+    <?php print $feed_icons ?>
+  </footer> <!-- /#footer -->
+
+</div> <!-- /#wrapper -->
